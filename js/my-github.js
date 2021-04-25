40ceb98113da47ca6534760089ec2fb524191c5d
Elir25 <elirendiles@gmail.com>
2021-04-25 12:36:36 +0200

    separate the tasks in functions

--- js/my-github.js
+++ js/my-github.js
@@ -1,35 +1,44 @@
 // Write code here to communicate with Github
-//parte 1, add the event listener
+//part 1, add the event listener
 document
   .querySelector(".nav-link.js-scroll-trigger")
   .addEventListener("click", getLisaRepos);
 
-function getLisaRepos(repoName) {
-  let ulElement = document.querySelector("#repos-list"); //get the ul Element
+function setRepoNumber(number) {
   let numOfRepos = document.querySelector("#repos-count"); //get the subheading (repos num)
+  numOfRepos.textContent = number;
+  console.log(numOfRepos.textContent);
+}
 
+function buildRepoList(repos) {
+  let ulElement = document.querySelector("#repos-list"); //get the ul Element
+
+  return repos.forEach(rep => {
+    const myLiElement = document.createElement("li");
+
+    const url = rep.html_url;
+
+    const a = document.createElement("a");
+    a.setAttribute("href", url);
+
+    let linkText = document.createTextNode(rep.name);
+
+    ulElement.appendChild(myLiElement);
+    myLiElement.appendChild(a);
+    a.appendChild(linkText);
+  });
+}
+
+function getLisaRepos(repoName) {
   fetch(`https://api.github.com/users/${repoName}/repos`)
     .then(response => response.json())
     .then(repos => {
       //show the number of repos
-      numOfRepos.textContent = repos.length;
-      console.log(numOfRepos.textContent);
+      setRepoNumber(repos.length);
 
       //get the repos inside the list element and create the links
-      return repos.forEach(rep => {
-        const myLiElement = document.createElement("li");
-
-        const url = rep.html_url;
-
-        const a = document.createElement("a");
-        a.setAttribute("href", url);
-
-        let linkText = document.createTextNode(rep.name);
-
-        ulElement.appendChild(myLiElement);
-        myLiElement.appendChild(a);
-        a.appendChild(linkText);
-      });
+      buildRepoList(repos);
     });
 }
+
 let myRepos = getLisaRepos("Elir25");
